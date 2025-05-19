487a08e06bd0f178a44132f371bbb1a86d2588e3
Andy Zhou <andyz229217@gmail.com>
2025-05-18 22:52:13 -0600

    I understand now

--- src/Car.py
+++ src/Car.py
@@ -1,17 +1,17 @@
 import State;
 
 class Car:
-    # Create the car AI
+    # Creates the car
     def _init__(self, action, reward, epsilon_start, epsilon_end, epsilon_decay):
         self.action = action
         self.reward = reward
         self.state = State(epsilon_start, epsilon_end, epsilon_decay)
-        
-    # Choose forward, left, right, or nothing using 
-    def chooseAction(self, epsilon_start, epsilon_end):
+
+    # Choose forward, left, right, or nothing using State
+    def chooseAction(self):
 
     # Do Action w/ action variable
-    def doAction(self, action):
+    def doAction(self):
 
-    # Updates the stats
-    def update(self, action, reward):
+    # Updates the stats of State and ReplayMemory
+    def update(self):
--- src/State.py
+++ src/State.py
@@ -1,5 +1,15 @@
+'''
+This program is supposd to handle all of the data and turn it into the next action
+'''
+
 class State:
     def __init__(self, epsilon_start, epsilon_end, epsilon_decay):
         self.epsilon_start = epsilon_start
         self.epsilon_end = epsilon_end
         self.epsilon_decay = epsilon_decay
+    
+    # Determines next action
+    def forward(self):
+
+    # Updates values
+    def update(self, epsilon_start, epsilon_end, epsilon_decay):
