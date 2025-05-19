07138b03123b6e259e6347823140381ff326950c
Andy Zhou <andyz229217@gmail.com>
2025-05-19 13:12:53 -0600

    Changed one line

--- src/Car.py
+++ src/Car.py
@@ -1,11 +1,13 @@
 import State;
+import ReplayMemory;
 
 class Car:
     # Creates the car
-    def _init__(self, action, reward, epsilon_start, epsilon_end, epsilon_decay):
+    def _init__(self, action, reward, epsilon_start, epsilon_end, epsilon_decay, replay_memory):
         self.action = action
         self.reward = reward
         self.state = State(epsilon_start, epsilon_end, epsilon_decay)
+        self.memory = replay_memory
 
     # Choose forward, left, right, or nothing using State
     def chooseAction(self):
